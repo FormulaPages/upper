5fa69919e267c1fae12e891e1c9ee19d690508db
Peter Moresi <peter.moresi@gmail.com>
2015-10-22 20:47:02 -0700

    Fix test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var upper = require('./UPPER');
 
-test('it should upper case a string', (t) => {
+test('it should upper case a string', function(t) {
   t.plan(3)
   t.equal( upper("lower"), "LOWER" )
   t.equal( upper("loWer"), "LOWER" )
